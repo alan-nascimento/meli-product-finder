651e3e1617c5c49fb2a336ac5ee9890a3d7f67df
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-08 19:10:56 -0300

    feat: remove input auto complete and handle input change

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -4,17 +4,25 @@ import { TextField } from '@material-ui/core';
 type Props = {
   name: string;
   value: string;
+  onChange: () => any;
   placeholder: string;
 };
 
-const Input: React.FC<Props> = ({ name, value, placeholder }: Props) => (
+const Input: React.FC<Props> = ({
+  name,
+  value,
+  onChange,
+  placeholder,
+}: Props) => (
   <TextField
     size="small"
     variant="outlined"
+    autoComplete="off"
     data-testid={`${name}-input`}
     id={name}
     name={name}
     value={value}
+    onChange={onChange}
     placeholder={placeholder}
   />
 );
