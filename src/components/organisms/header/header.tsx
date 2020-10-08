f938b09fae00943802e76997cf10d57afcaf1090
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-08 19:17:52 -0300

    feat: set search query to url

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import debounce from 'lodash.debounce';
+import { useRouter } from 'next/router';
 
 import { SearchForm, Logo } from '@/components/molecules';
 
 import { Container, Content } from './header.styles';
 
-const Header: React.FC = () => (
-  <Container>
-    <Content>
-      <Logo />
-      <SearchForm value="" onSearch={() => ({})} />
-    </Content>
-  </Container>
-);
+const Header: React.FC = () => {
+  const { query, push } = useRouter();
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    if (query.search) {
+      setSearch(`${query.search}`);
+    }
+  }, [setSearch]);
+
+  const debounceOnSearch = debounce((value: string) => {
+    push(`/items?search=${value}`);
+  }, 800);
+
+  const handleSearch = (value: string) => value && debounceOnSearch(value);
+
+  return (
+    <Container>
+      <Content>
+        <Logo />
+        <SearchForm value={search} onSearch={handleSearch} />
+      </Content>
+    </Container>
+  );
+};
 
 export default Header;
